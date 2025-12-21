7dd92d8c34cf9a33a63b448d44d4c1744c17f495
VanTi <143702408+IDTiK5@users.noreply.github.com>
2025-12-21 15:39:30 +0700

    Refactor ESP box configuration options

--- Module/ESPBox.lua/BoxElement.lua
+++ Module/ESPBox.lua/BoxElement.lua
@@ -1,50 +1,41 @@
-boxTab:Toggle({
-	Name = "Enable ESP",
-	Flag = "ESP_Toggle",
-	Default = true,
-	Callback = function(value)
-		ESP.config.enabled = value
-	end
-})
-
-boxTab:Slider({
-	Name = "Max Distance",
-	Flag = "MaxDistance",
-	Min = 0,
-	Max = 10000,
-	Default = 500,
-	Callback = function(value)
-		ESP.config.maxDistance = value
-	end
-})
-
-boxTab:Dropdown({
-	Name = "Box Type",
-	Flag = "BoxType",
-	Items = {"2D Box", "3D Box", "Corner Box"},
-	Default = "2D Box",
-	Callback = function(value)
-		if value == "2D Box" then
-			ESP.config.box3D = false
-			ESP.config.cornerBox = false
-		elseif value == "3D Box" then
-			ESP.config.box3D = true
-			ESP.config.cornerBox = false
-		elseif value == "Corner Box" then
-			ESP.config.box3D = false
-			ESP.config.cornerBox = true
+return function(boxTab, ESP)
+	boxTab:Toggle({
+		Name = "Enable ESP",
+		Flag = "ESP_Toggle",
+		Default = true,
+		Callback = function(value)
+			ESP.config.enabled = value
 		end
-		ESP:ForceResort()
-	end
-})
-
-boxTab:Dropdown({
-	Name = "Team Filter Mode",
-	Flag = "TeamFilterMode",
-	Items = {"standard", "advanced"},
-	Default = "standard",
-	Callback = function(value)
-		ESP.config.teamFilterMode = value
-		ESP:Refresh()
-	end
-})
+	})
+	
+	boxTab:Dropdown({
+		Name = "Loại hộp",
+		Flag = "BoxType",
+		Items = {"Hộp 2D", "Hộp 3D", "Hộp góc"},
+		Default = "Hộp 2D",
+		Callback = function(value)
+			if value == "Hộp 2D" then
+				ESP.config.box3D = false
+				ESP.config.cornerBox = false
+			elseif value == "Hộp 3D" then
+				ESP.config.box3D = true
+				ESP.config.cornerBox = false
+			elseif value == "Hộp góc" then
+				ESP.config.box3D = false
+				ESP.config.cornerBox = true
+			end
+			ESP:ForceResort()
+		end
+	})
+	
+	boxTab:Slider({
+		Name = "Max Distance",
+		Flag = "MaxDistance",
+		Min = 0,
+		Max = 10000,
+		Default = 10000,
+		Callback = function(value)
+			ESP.config.maxDistance = value
+		end
+	})
+end
